2b01cd85293c314f901cb39e802c0b43d11bc925
cviguers <cviguers@gmail.com>
2023-03-26 16:24:42 -0400

    local history

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,18 +13,17 @@ var forecastHeaderEl = document.querySelector(".forecastHeader");
 var historyList = document.querySelector(".history-container");
 historyList.textContent = "Search History";
 
-
 // show forecast header
 var renderForecastHeader = function () {
-  var forecastHeading = document.createElement('h4')
-  forecastHeading.textContent = "5 Day Forecast"
-  forecastHeaderEl.append(forecastHeading)
-}
+  var forecastHeading = document.createElement("h4");
+  forecastHeading.textContent = "5 Day Forecast";
+  forecastHeaderEl.append(forecastHeading);
+};
 
 // show 5 day forecast
 var renderForecast = function (data) {
   // converts each forecast into var arr
-  renderForecastHeader()
+  renderForecastHeader();
   var arr = data.list;
   for (var i = 0; i < arr.length; i++) {
     // splitting the array so you can only see each day at 12
@@ -34,20 +33,26 @@ var renderForecast = function (data) {
       forecastContainerEl.append(fiveDay);
       var fiveDayCard = document.createElement("ul");
       fiveDay.append(fiveDayCard);
+      var time5El = document.createElement("li");
       var temp5El = document.createElement("li");
       var wind5El = document.createElement("li");
       var humidity5El = document.createElement("li");
       var icon5El = document.createElement("img");
+      time5El.textContent = arr[i].dt_txt;
       var icon5 = arr[i].weather[0].icon;
-      icon5El.setAttribute("src", "http://openweathermap.org/img/w/" + icon5 + ".png")
-      temp5El.textContent = "temp: " + arr[i].main.temp  + "°F";
+      icon5El.setAttribute(
+        "src",
+        "http://openweathermap.org/img/w/" + icon5 + ".png"
+      );
+      temp5El.textContent = "temp: " + arr[i].main.temp + "°F";
       wind5El.textContent = "wind: " + arr[i].wind.speed + "MPH";
       humidity5El.textContent = "humidity: " + arr[i].main.humidity + "%";
       fiveDayCard.style.border = "2px solid navy";
       fiveDayCard.style.display = "inline-block";
       fiveDayCard.style.backgroundColor = "navy";
-      fiveDayCard.style.color = "white"
-      fiveDayCard.style.margin = "5px"
+      fiveDayCard.style.color = "white";
+      fiveDayCard.style.margin = "5px";
+      fiveDayCard.append(time5El);
       fiveDayCard.append(temp5El);
       fiveDayCard.append(wind5El);
       fiveDayCard.append(humidity5El);
@@ -69,17 +74,16 @@ var renderCurrentWeather = function (data) {
 
     // putting api weather info on page
     currentHeader.textContent = cityNameInput + " " + time;
-    currentIconInput.setAttribute("src", "http://openweathermap.org/img/w/" + icon + ".png");
+    currentIconInput.setAttribute(
+      "src",
+      "http://openweathermap.org/img/w/" + icon + ".png"
+    );
     currentTempInput.textContent = "temp: " + temp + "°F";
     ("F");
     currentWindInput.textContent = "wind: " + wind + "MPH";
     currentHumidityInput.textContent = "humidity: " + humidity + "%";
     currentWeatherEl.classList.add("currentContainer");
   }
-};
-
-var toJSON = function (response) {
-  return response.json();
 };
 
 var renderLatLon = function (data) {
@@ -100,7 +104,6 @@ var renderLatLon = function (data) {
         return response.json();
       })
       .then(function (data) {
-        // setSearchedWeather(data);
         renderCurrentWeather(data);
         renderForecast(data);
       })
@@ -130,15 +133,52 @@ var fetchCity = function () {
     });
 };
 
-var cityList = document.querySelector("#history")
 // call functions on click and collect user input for api/local storage
-searchBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    var searchValue = cityNameInput.value.toLowerCase();
-    fetchCity();
-    renderLocalHistory()
-})
+searchBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  var searchValue = cityNameInput.value;
+  fetchCity();
+  setSeachedHistory(searchValue);
+  getSearchedHistory(searchValue);
+});
 
-var renderLocalHistory = function() {
+// receive local storage and apply to page
+var getSearchedHistory = function () {
+  var localHistory = JSON.parse(localStorage.getItem("searchedCities")) || [];
+  for (var i = 0; i < localHistory.length; i++) {
+    var searchTerm = localHistory[i];
+    var newLi = document.createElement("button");
+    newLi.textContent = searchTerm;
+    newLi.classList.add("btn");
+    newLi.classList.add("searchbtn");
+    newLi.style.display = "block";
+    historyList.appendChild(newLi);
+    newLi.addEventListener("click", function(){
+      var fetchCityClick = function () {
+        cityNameInput = searchTerm;
+        var cityURL =
+          "http://api.openweathermap.org/geo/1.0/direct?q=" +
+          cityNameInput +
+          "&limit=1&appid=b89c09787bf9106df63088418a47c76b";
+        fetch(cityURL)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            renderLatLon(data);
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+      };
+    })
+  }
+};
 
-}
+// set up local storage keys
+var setSeachedHistory = function (text) {
+  var searchBarHistory =
+    JSON.parse(localStorage.getItem("searchedCities")) || [];
+  searchBarHistory.push(text);
+  localStorage.setItem("searchedCities", JSON.stringify(searchBarHistory));
+};
